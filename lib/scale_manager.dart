7fff9a45d7ae6b3b23b5f896a0916b31277d9cdb
Anirudh Singh <anirudhisgreat1200@gmail.com>
2021-06-20 21:42:03 +0530

    updated directory structure

--- lib/scale_manager.dart
+++ lib/scale_manager.dart
@@ -1,31 +1,3 @@
 library scale_manager;
 
-import 'package:flutter/material.dart';
-
-class ScaleManager {
-  /// Basic [BuildContext] of your app
-  final BuildContext context;
-
-  /// Device mockup width used in Figma
-  /// PS: Hover on any Figma mockup screen to get it
-  final double kMockupWidth;
-
-  ScaleManager({
-    @required this.context,
-    @required this.kMockupWidth,
-  });
-
-  /// Returns the current [deviceWidth] fetched via [MediaQuery]
-  double get deviceWidth => MediaQuery.of(context).size.width;
-
-  /// Returns the calculated [imageFactor] which should be used in Image parts
-  double get imageFactor => kMockupWidth / deviceWidth;
-
-  /// Returns the calculated [textScaleFactor] which should be used in Text styling
-  double get textScaleFactor => deviceWidth / kMockupWidth;
-
-  /// Returns the calculated [spaceScale] which should be used in
-  /// places like Padding or Margin values, SizedBox values or other const places
-  double spaceScale({@required double space}) =>
-      space / kMockupWidth * deviceWidth;
-}
+export 'src/scale_manager_impl.dart';
